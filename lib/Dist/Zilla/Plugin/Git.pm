590038b0cfb084538b75d06e5e39fff77c14cb39
Jérôme Quelin <jquelin@gmail.com>
2009-11-21 18:22:56 +0100

    pod update

--- lib/Dist/Zilla/Plugin/Git.pm
+++ lib/Dist/Zilla/Plugin/Git.pm
@@ -8,75 +8,28 @@ package Dist::Zilla::Plugin::Git;
 1;
 __END__
 
-=head1 SYNOPSIS
-
-In your F<dist.ini>:
-
-    [Git]
-    filename = Changes      ; this is the default
-
 
 =head1 DESCRIPTION
 
-This plugin does two things for module authors using L<git|http://git-
-scm.com> to track their work:
+This set of plugins for L<Dist::Zilla> can do interesting things for
+module authors using L<git|http://git- scm.com> to track their work. The
+following plugins are provided in this distribution:
 
 =over 4
 
-=item * before releasing: checks that git is in a clean state
+=item * L<Dist::Zilla::Plugin::Git::Check>
 
-=item * after releasing: perform some simple git actions
+=item * L<Dist::Zilla::Plugin::Git::Commit>
+
+=item * L<Dist::Zilla::Plugin::Git::Tag>
+
+=item * L<Dist::Zilla::Plugin::Git::Push>
 
 =back
 
 
-The plugin accepts the following options:
-
-=over 4
-
-=item * filename - the name of your changelog file. defaults to F<Changes>.
-
-=back
-
-
-=head2 Checks before releasing
-
-The following checks are performed before releasing:
-
-=over 4
-
-=item * there should be no files in the index (staged copy)
-
-=item * there should be no untracked files in the working copy
-
-=item * the working copy should be clean. The changelog and F<dist.ini>
-can be modified locally, though.
-
-=back
-
-If those conditions are not met, the plugin will die, and the release
-will thus be aborted. This lets you fix the problems before continuing.
-
-
-=head2 Simple git actions after releasing
-
-Once the release is done, this plugin will record this fact in git. The
-following actions are then performed:
-
-=over 4
-
-=item * commit your changelog (and your dzil config if you update the
-version manually) to git. The commit message will be the changelog entry
-for this release.
-
-=item * create a tag named C<v$VERSION>.
-
-=item * push the branch and the tags to your remote repository. Since
-it's a simple push, it means that the remotes should be correctly
-configured in your local repository.
-
-=back
-
+If you want to use all of them at once, you will be interested by
+L<Dist::Zilla::PluginBundle::Git>.
 
 
 
