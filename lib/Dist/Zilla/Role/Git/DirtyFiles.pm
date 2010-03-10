b6f4789078dee637982a11d46edf36d52b8012d5
Jérôme Quelin <jquelin@gmail.com>
2010-03-10 10:07:55 +0100

    pod update

--- lib/Dist/Zilla/Role/Git/DirtyFiles.pm
+++ lib/Dist/Zilla/Role/Git/DirtyFiles.pm
@@ -10,24 +10,40 @@ use Moose::Autobox;
 use MooseX::Has::Sugar;
 use MooseX::Types::Moose qw{ ArrayRef Str };
 
+
+# -- attributes
+
+=attr allow_dirty
+
+A list of files that are allowed to be dirty in the git checkout.
+Defaults to C<dist.ini> and the changelog (as defined per the
+C<filename> attribute.
+
+=attr filename
+
+The name of the changelog. Defaults to C<Changes>.
+
+=cut
+
 has allow_dirty => (
   ro, lazy,
   isa     => ArrayRef[Str],
   builder => '_build_allow_dirty',
 );
-
 has filename => ( ro, isa=>Str, default => 'Changes' );
-
-sub _build_allow_dirty { [ 'dist.ini', shift->filename ] }
 
 sub mvp_multivalue_args { qw(allow_dirty) }
 
-=for Pod::Coverage::TrustPod
-    mvp_multivalue_args
+
+# -- builders & initializers
+
+sub _build_allow_dirty { [ 'dist.ini', shift->filename ] }
+
+
 
 =method list_dirty_files
 
-  @dirty = $plugin->list_dirty_files($git, $listAllowed);
+  my @dirty = $plugin->list_dirty_files($git, $listAllowed);
 
 This returns a list of the modified or deleted files in C<$git>,
 filtered against the C<allow_dirty> attribute.  If C<$listAllowed> is
@@ -48,6 +64,16 @@ sub list_dirty_files
       $git->ls_files( { modified=>1, deleted=>1 } );
 } # end list_dirty_files
 
+
 no Moose::Role;
 no MooseX::Has::Sugar;
 1;
+__END__
+
+=for Pod::Coverage::TrustPod
+    mvp_multivalue_args
+
+=head1 DESCRIPTION
+
+This role is used within the git plugin to work with files that are
+dirty in the local git checkout.
