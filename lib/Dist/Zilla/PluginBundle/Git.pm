61b0f64e1dc85a40441b1156c0fc29f95ddd7b1b
Graham Barr <gbarr@pobox.com>
2010-04-01 12:23:49 -0500

    Make PluginBundle::Git aware of multi value args of its components

--- lib/Dist/Zilla/PluginBundle/Git.pm
+++ lib/Dist/Zilla/PluginBundle/Git.pm
@@ -10,20 +10,36 @@ use Class::MOP;
 
 with 'Dist::Zilla::Role::PluginBundle';
 
+# bundle all git plugins
+my @names   = qw{ Check Commit Tag Push };
+
+# bundle all git plugins
+my @names   = qw{ Check Commit Tag Push };
+
+my %multi;
+for my $name (@names) {
+    my $class = "Dist::Zilla::Plugin::Git::$name";
+    Class::MOP::load_class($class);
+    @multi{$class->mvp_multivalue_args} = ()
+        if $class->can('mvp_multivalue_args');
+}
+
+sub mvp_multivalue_args { keys %multi; }
+
 sub bundle_config {
     my ($self, $section) = @_;
     my $class = ( ref $self ) || $self;
     my $arg   = $section->{payload};
 
-    # bundle all git plugins
-    my @names   = qw{ Check Commit Tag Push };
-
     my @config;
 
     for my $name (@names) {
         my $class = "Dist::Zilla::Plugin::Git::$name";
-        Class::MOP::load_class($class);
-        push @config, [ "$section->{name}/$name" => $class => $arg ];
+        my %payload;
+        foreach my $k (keys %$arg) {
+            $payload{$k} = $arg->{$k} if $class->can($k);
+        }
+        push @config, [ "$section->{name}/$name" => $class => \%payload ];
     }
 
     return @config;
