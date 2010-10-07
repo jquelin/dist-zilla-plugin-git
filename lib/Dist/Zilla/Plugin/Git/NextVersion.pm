8ed979d373f688833ef2d9612eade5c09c3b7ac5
David Golden <dagolden@cpan.org>
2010-10-07 13:58:08 -0400

    Add Git::NextVersion

---
+++ lib/Dist/Zilla/Plugin/Git/NextVersion.pm
@@ -0,0 +1,99 @@
+use strict;
+use warnings;
+
+package Dist::Zilla::Plugin::Git::NextVersion;
+# ABSTRACT: provide a version number by bumping the last git release tag
+
+use Dist::Zilla 4 ();
+use Git::Wrapper;
+use Version::Next ();
+use version 0.80 ();
+
+use Moose;
+use namespace::autoclean 0.09;
+
+with 'Dist::Zilla::Role::VersionProvider';
+
+# -- attributes
+
+has version_regexp  => ( is => 'ro', isa=>'Str', default => '^v(.+)$' );
+
+has first_version  => ( is => 'ro', isa=>'Str', default => '0.001' );
+
+# -- role implementation
+
+sub provide_version {
+  my ($self) = @_;
+
+  # override (or maybe needed to initialize)
+  return $ENV{V} if exists $ENV{V};
+
+  my $git  = Git::Wrapper->new('.');
+  my $regexp = $self->version_regexp;
+
+  my @tags = $git->tag;
+  return $self->first_version unless @tags;
+
+  # find highest version from tags
+  my ($last_ver) =  sort { version->parse($b) <=> version->parse($a) }
+  grep { eval { version->parse($_) }  }
+  map  { /$regexp/ ? $1 : ()          } @tags;
+
+  $self->log_fatal("Could not determine last version from tags")
+  unless defined $last_ver;
+
+  my $new_ver = Version::Next::next_version($last_ver);
+  $self->log("Bumping version from $last_ver to $new_ver");
+
+  $self->zilla->version("$new_ver");
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
+
+__END__
+
+=for Pod::Coverage
+    provide_version
+
+=head1 SYNOPSIS
+
+In your F<dist.ini>:
+
+    [Git::NextVersion]
+    first_version = 0.001       ; this is the default
+    version_regexp  = ^v(.+)$   ; this is the default
+
+=head1 DESCRIPTION
+
+This does the L<Dist::Zilla::Role::VersionProvider> role.  It finds the last
+version number from your git tags, increments it using L<Version::Next>, and
+uses the result as the C<version> parameter for your distribution.
+
+The plugin accepts the following options:
+
+=over
+
+=item *
+
+C<first_version> - if the repository has no tags at all, this version
+is used as the first version for the distribution.  It defaults to "0.001".
+
+=item *
+
+C<version_regexp> - regular expression that matches a tag containing
+a version.  It must capture the version into $1.  Defaults to ^v(.+)$
+which matches the default C<tag_format> from L<Dist::Zilla::Plugin::Git::Tag>.
+If you change C<tag_format>, you B<must> set a corresponsing C<version_regexp>.
+
+=back
+
+You can also set the C<V> environment variable to override the new version.
+This is useful if you need to bump to a specific version.  For example, if
+the last tag is 0.005 and you want to jump to 1.000 you can set V = 1.000.
+
+  $ V=1.000 dzil release
+
+=cut
+
---
+++ t/5-version-default.t
@@ -0,0 +1,79 @@
+#!perl
+
+use strict;
+use warnings;
+use lib 't/lib';
+
+use Dist::Zilla::Tester;
+use Git::Wrapper;
+use Path::Class;
+use File::pushd qw/pushd tempd/;
+use File::Copy::Recursive qw/dircopy/;
+
+use Test::More 0.88 tests => 6;
+
+# we chdir around so make @INC absolute
+BEGIN { 
+  @INC = map {; ref($_) ? $_ : dir($_)->absolute->stringify } @INC;
+}
+
+# save absolute corpus directory path
+my $corpus_dir = dir('corpus/version-default')->absolute;
+
+# isolate repo directory from possible git actions from bugs
+my $tempd = tempd;
+
+## shortcut for new tester object
+sub _new_zilla {
+  my $root = shift;
+  return Dist::Zilla::Tester->from_config({
+    dist_root => $corpus_dir,
+  });
+}
+
+## Tests start here
+
+my ($zilla, $version);
+$zilla = _new_zilla;
+# enter the temp source dir and make it a git dir
+my $wd = pushd( $zilla->tempdir->subdir('source')->stringify );
+
+system "git init";
+my $git   = Git::Wrapper->new('.');
+$git->add(".");
+$git->commit({ message => 'import' });
+
+# with no tags and no initialization, should get default
+$zilla = _new_zilla;
+$version = $zilla->version;
+is( $version, "0.001", "default is 0.001" );
+
+# initialize it
+{
+    local $ENV{V} = "1.23";
+    $zilla = _new_zilla;
+    is( $zilla->version, "1.23", "initialized with \$ENV{V}" );
+}
+
+# tag it
+$git->tag("v1.2.3");
+ok( (grep { /v1\.2\.3/ } $git->tag), "wrote v1.2.3 tag" );
+
+{
+    $zilla = _new_zilla;
+    is( $zilla->version, "1.2.4", "initialized from last tag" );
+}
+
+# tag it
+$git->tag("v1.23");
+ok( (grep { /v1\.23/ } $git->tag), "wrote v1.23 tag" );
+
+{
+    $zilla = _new_zilla;
+    is( $zilla->version, "1.24", "initialized from last tag" );
+}
+
+
+
+done_testing;
+
