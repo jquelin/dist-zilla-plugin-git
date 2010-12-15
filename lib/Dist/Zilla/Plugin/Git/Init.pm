3e3599601a4e887643ab814d99653f43a94fa672
Christopher J. Madsen <perl@cjmweb.net>
2010-12-15 16:10:57 -0600

    add remote option to Init

--- lib/Dist/Zilla/Plugin/Git/Init.pm
+++ lib/Dist/Zilla/Plugin/Git/Init.pm
@@ -5,8 +5,21 @@ use warnings;
 package Dist::Zilla::Plugin::Git::Init;
 # ABSTRACT: initialize git repository on dzil new
 
+our %transform = (
+  lc => sub { lc shift },
+  uc => sub { uc shift },
+  '' => sub { shift },
+);
+
 use Moose;
 use Git::Wrapper;
+use String::Formatter method_stringf => {
+  -as => '_format_string',
+  codes => {
+    n => sub { "\n" },
+    N => sub { $transform{$_[1] || ''}->( $_[0]->zilla->name ) },
+  },
+};
 
 with 'Dist::Zilla::Role::AfterMint';
 
@@ -16,6 +29,15 @@ has commit_message => (
     default => 'initial commit',
 );
 
+has remotes => (
+  is   => 'ro',
+  isa  => 'ArrayRef[Str]',
+  default => sub { [] },
+);
+
+sub mvp_multivalue_args { qw(remotes) }
+sub mvp_aliases { return { remote => 'remotes' } }
+
 sub after_mint {
     my $self = shift;
     my ($opts) = @_;
@@ -23,17 +45,63 @@ sub after_mint {
     $self->log("Initializing a new git repository in " . $opts->{mint_root});
     $git->init;
     $git->add($opts->{mint_root});
-    $git->commit({message => $self->commit_message});
+    $git->commit({message => _format_string($self->commit_message, $self)});
+    foreach my $remoteSpec (@{ $self->remotes }) {
+      my ($remote, $url) = split ' ', _format_string($remoteSpec, $self), 2;
+      $self->log("Adding remote $remote as $url");
+      $git->remote(add => $remote, $url);
+    }
 }
 
 1;
 __END__
 
 =for Pod::Coverage
-    after_mint
+    after_mint mvp_aliases mvp_multivalue_args
 
+
+=head1 SYNOPSIS
+
+In your F<profile.ini>:
+
+    [Git::Init]
+    commit_message = initial commit  ; this is the default
+    remote = origin git@github.com:USERNAME/%{lc}N.git ; no default
 
 =head1 DESCRIPTION
 
 This plugin initializes a git repository when a new distribution is
 created with C<dzil new>.
+
+
+=head2 Plugin options
+
+The plugin accepts the following options:
+
+=over 4
+
+=item * commit_message - the commit message to use when checking in
+the newly-minted dist. Defaults to C<initial commit>.
+
+=item * remote - a remote to add to the repository.  No remotes are
+added by default.  A remote is specified as C<NAME URL>.  This may be
+specified multiple times to add multiple remotes.
+
+=back
+
+=head2 Formatting options
+
+You can use the following codes in C<commit_message> or C<remote>:
+
+=over 4
+
+=item C<%n>
+
+A newline.
+
+=item C<%N>
+
+The distribution name.  You can also use C<%{lc}N> or C<%{uc}N> to get
+the name in lower case or upper case, respectively.
+
+=back
