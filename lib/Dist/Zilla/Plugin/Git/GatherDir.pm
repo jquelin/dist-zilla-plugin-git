504386fed6045d18a054e39c8383aeeab1fbee94
Geoffrey Broadwell <geoffb@corp.sonic.net>
2011-02-04 21:27:37 -0800

    Proof of concept Dist::Zilla::Plugin::Git::GatherDir
    It is based almost entirely on the original Dist::Zilla::Plugin::GatherDir,
    tweaked to use 'git ls-files' via Git::Wrapper instead of searching with
    File::Find::Rule

---
+++ lib/Dist/Zilla/Plugin/Git/GatherDir.pm
@@ -0,0 +1,130 @@
+package Dist::Zilla::Plugin::Git::GatherDir;
+# ABSTRACT: gather all tracked files in a Git working directory
+use Moose;
+use Moose::Autobox;
+use MooseX::Types::Path::Class qw(Dir File);
+with 'Dist::Zilla::Role::FileGatherer';
+
+=head1 DESCRIPTION
+
+This is a trivial variant of the L<GatherDir|Dist::Zilla::Plugin::GatherDir>
+plugin.  It looks in the directory named in the L</root> attribute and adds all
+the Git tracked files it finds there (as determined by C<git ls-files>).  If the
+root begins with a tilde, the tilde is replaced with the current user's home
+directory according to L<File::HomeDir>.
+
+Most users just need:
+
+  [Git::GatherDir]
+
+...and this will pick up all tracked files from the current directory into the
+dist.  You can use it multiple times, as you can any other plugin, by providing
+a plugin name.  For example, if you want to include external specification
+files into a subdir of your dist, you might write:
+
+  [Git::GatherDir]
+  ; this plugin needs no config and gathers most of your files
+
+  [Git::GatherDir / SpecFiles]
+  ; this plugin gets all tracked files in the root dir and adds them under ./spec
+  root   = ~/projects/my-project/spec
+  prefix = spec
+
+=cut
+
+use Git::Wrapper;
+use File::HomeDir;
+use File::Spec;
+use Path::Class;
+
+use namespace::autoclean;
+
+=attr root
+
+This is the directory in which to look for files.  If not given, it defaults to
+the dist root -- generally, the place where your F<dist.ini> or other
+configuration file is located.
+
+=cut
+
+has root => (
+  is   => 'ro',
+  isa  => Dir,
+  lazy => 1,
+  coerce   => 1,
+  required => 1,
+  default  => sub { shift->zilla->root },
+);
+
+=attr prefix
+
+This parameter can be set to gather all the files found under a common
+directory.  See the L<description|DESCRIPTION> above for an example.
+
+=cut
+
+has prefix => (
+  is  => 'ro',
+  isa => 'Str',
+  default => '',
+);
+
+=attr include_dotfiles
+
+By default, files will not be included if they begin with a dot.  This goes
+both for files and for directories relative to the C<root>.
+
+In almost all cases, the default value (false) is correct.
+
+=cut
+
+has include_dotfiles => (
+  is  => 'ro',
+  isa => 'Bool',
+  default => 0,
+);
+
+sub gather_files {
+  my ($self) = @_;
+
+  my $root = "" . $self->root;
+  $root =~ s{^~([\\/])}{File::HomeDir->my_home . $1}e;
+  $root = Path::Class::dir($root);
+
+  my $git = Git::Wrapper->new($root);
+
+  my @files;
+  FILE: for my $filename ($git->ls_files) {
+    unless ($self->include_dotfiles) {
+      my $file = file($filename)->relative($root);
+      next FILE if $file->basename =~ qr/^\./;
+      next FILE if grep { /^\.[^.]/ } $file->dir->dir_list;
+    }
+
+    push @files, $self->_file_from_filename($filename);
+  }
+
+  for my $file (@files) {
+    (my $newname = $file->name) =~ s{\A\Q$root\E[\\/]}{}g;
+    $newname = File::Spec->catdir($self->prefix, $newname) if $self->prefix;
+    $newname = Path::Class::dir($newname)->as_foreign('Unix')->stringify;
+
+    $file->name($newname);
+    $self->add_file($file);
+  }
+
+  return;
+}
+
+sub _file_from_filename {
+  my ($self, $filename) = @_;
+
+  return Dist::Zilla::File::OnDisk->new({
+    name => $filename,
+    mode => (stat $filename)[2] & 0755, # kill world-writeability
+  });
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
