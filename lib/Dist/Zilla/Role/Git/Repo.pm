c93566f0c0632070503a7883b7425256d8730f2f
Mike Friedman <friedo@socialflow.com>
2011-11-16 16:48:00 +0000

    cleanup and docs

--- lib/Dist/Zilla/Role/Git/Repo.pm
+++ lib/Dist/Zilla/Role/Git/Repo.pm
@@ -1,7 +1,24 @@
 package Dist::Zilla::Role::Git::Repo;
 
+# ABSTRACT: Provide repository information for Git plugins
+
 use Moose::Role;
 
 has 'repo_root'   => ( is => 'ro', isa => 'Str', default => '.' );
 
+
 1;
+
+
+__END__
+
+=pod
+
+=head1 DESCRIPTION
+
+This role is used within the git plugin to get information about the repository structure.
+
+=attr repo_root
+
+The repository root, either as a full path or relative to the distribution root. Default is C<.>.
+
